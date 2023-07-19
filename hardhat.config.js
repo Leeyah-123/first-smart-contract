c260385b9e98099b8733d0c1e9c515b33bddef08
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-07-19 21:08:30 +0100

    refactor: Switch from goerli testnet to sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,10 +8,6 @@ require('@nomiclabs/hardhat-truffle5');
 module.exports = {
   solidity: '0.8.19',
   networks: {
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: { mnemonic: process.env.MNEMONIC },
-    },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${'Jm3a7O_i7BhOb0WpD82wCz55ovxUV605'}`,
       accounts: [process.env.SEPOLIA_PRIVATE_KEY],
