42567834369926bdd3c1575e53d4c815fa7d36b2
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-08-06 17:51:18 +0100

    rate: Calculate current rate of exchange

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -2,82 +2,131 @@
 pragma solidity ^0.8.0;
 
 contract Account {
-  address private _owner;
-  address[] private _accountOwners;
-  uint256 private _rewardInterval = 1;
+    address private _owner;
+    uint256 private _totalTokenSupply;
+    uint256 private _totalRewards;
 
-  mapping(address => AccountInfo) public ownerToAccountInfo;
-  uint256 public rate = 1000;
+    bool internal locked;
 
-  struct AccountInfo {
-    uint256 balance;
-    uint256 createdAt;
-  }
+    mapping(address => AccountInfo) ownerToAccountInfo;
 
-  constructor() {
-    _owner = msg.sender;
-  }
-
-  function distributeReward() external payable {
-    require(msg.sender == _owner);
-
-    uint256 tokens = msg.value * rate;
-
-    uint256 numOfEligibleAccounts;
-
-    for (uint256 i = 0; i > _accountOwners.length; i++) {
-      if ( // check if account creation date is greater than reward interval
-        ownerToAccountInfo[_accountOwners[i]].createdAt <=
-        (block.timestamp - (_rewardInterval * 1 days))
-      ) {
-        numOfEligibleAccounts++;
-      }
+    struct AccountInfo {
+        uint256 balance;
+        uint256 createdAt;
+        // uint256 lastRewardIndex;
+        uint256 rewardTokens;
     }
 
-    uint256 numOfTokensForEach = tokens / numOfEligibleAccounts;
-
-    for (uint256 i = 0; i > _accountOwners.length; i++) {
-      if ( // send reward to eligible accounts
-        ownerToAccountInfo[_accountOwners[i]].createdAt <=
-        (block.timestamp - (_rewardInterval * 1 days))
-      ) {
-        ownerToAccountInfo[_accountOwners[i]].balance += numOfTokensForEach;
-      }
-    }
-  }
-
-  function deposit() external payable {
-    require(msg.value > 0, "Amount to deposit must be more than 0");
-
-    if (ownerToAccountInfo[msg.sender].createdAt == 0) { // save new account
-      ownerToAccountInfo[msg.sender] = AccountInfo(0, block.timestamp);
-      _accountOwners.push(msg.sender);
+    constructor() {
+        _owner = msg.sender;
     }
 
-    ownerToAccountInfo[msg.sender].balance += (msg.value * rate);
-  }
+    modifier noReentrant() {
+        require(!locked, "cannot reenter");
+        locked = true;
+        _;
+        locked = false;
+    }
 
-  function transfer(uint256 amount, address recipient) external {
-    require(
-      ownerToAccountInfo[msg.sender].balance >= amount,
-      "Insufficient balance"
-    );
+    modifier accountBalanceGreaterThanOrEqualTo(
+        address _accountOwner,
+        uint256 _amount
+    ) {
+        require(
+            ownerToAccountInfo[_accountOwner].balance >= _amount,
+            "Insufficient balance"
+        );
+        _;
+    }
 
-    ownerToAccountInfo[msg.sender].balance -= amount;
-    ownerToAccountInfo[recipient].balance += amount;
-  }
+    // rate refers to the amount of tokens equal to 1 ether
+    function _getRate() private view returns (uint256) {
+        // setting initial rate
+        if (_totalTokenSupply == 0) return 1000;
+        else {
+            uint256 rate = (1 * _totalTokenSupply) / address(this).balance;
+            return rate;
+        }
+    }
 
-  function burn(uint256 amount) external {
-    require(ownerToAccountInfo[msg.sender].balance >= amount, "Insufficient balance");
+    function _getTokensFromEth(
+        uint256 ethAmount
+    ) private view returns (uint256) {
+        return (ethAmount * _getRate());
+    }
 
-    ownerToAccountInfo[msg.sender].balance -= amount;
-  }
+    function _getEthFromTokens(
+        uint256 tokenAmount
+    ) private view returns (uint256) {
+        return (tokenAmount / _getRate());
+    }
 
-  function withdraw(uint256 amount) external {
-    require(ownerToAccountInfo[msg.sender].balance > (amount * rate), "Insufficient balance");
+    function _updateUserRewards() private {
+        if (ownerToAccountInfo[msg.sender].rewardTokens > 0) {
+            uint256 balanceWithRewards = (ownerToAccountInfo[msg.sender]
+                .rewardTokens * _totalRewards) / 100;
+            ownerToAccountInfo[msg.sender].rewardTokens = 0;
+            ownerToAccountInfo[msg.sender].balance += balanceWithRewards;
+        }
+    }
 
-    uint256 amountToPay = amount / rate;
-    ownerToAccountInfo[msg.sender].balance -= amount;
-    payable(msg.sender).transfer(amountToPay);
-  }
+    // TODO: Calculate how much token each person will receive
+    function distributeReward() external payable {
+        require(msg.sender == _owner);
+        uint256 tokens = _getTokensFromEth(msg.value);
+        _totalRewards += tokens;
+    }
+
+    function deposit() external payable {
+        require(
+            msg.value > 1 ether,
+            "Amount to deposit must be more than 1 ether"
+        );
+
+        if (ownerToAccountInfo[msg.sender].createdAt == 0) {
+            // save new account
+            ownerToAccountInfo[msg.sender] = AccountInfo(
+                0,
+                block.timestamp,
+                msg.value
+            );
+        }
+
+        uint256 tokenAmount = _getTokensFromEth(msg.value);
+        _totalTokenSupply += tokenAmount;
+        ownerToAccountInfo[msg.sender].balance += tokenAmount;
+    }
+
+    function transfer(
+        uint256 _amount,
+        address recipient
+    )
+        external
+        noReentrant
+        accountBalanceGreaterThanOrEqualTo(msg.sender, _amount)
+    {
+        ownerToAccountInfo[msg.sender].balance += _amount;
+        ownerToAccountInfo[recipient].balance += _amount;
+    }
+
+    function burn(
+        uint256 _amount
+    ) external accountBalanceGreaterThanOrEqualTo(msg.sender, _amount) {
+        ownerToAccountInfo[msg.sender].balance -= _amount;
+    }
+
+    function withdraw(
+        uint256 _amount
+    )
+        external
+        noReentrant
+        accountBalanceGreaterThanOrEqualTo(msg.sender, _amount)
+    {
+        _updateUserRewards();
+        uint256 amountToReceive = _getTokensFromEth(_amount);
+        ownerToAccountInfo[msg.sender].balance -= amountToReceive;
+
+        (bool success, ) = msg.sender.call{value: amountToReceive}("");
+        require(success, "transaction failed");
+    }
 }
